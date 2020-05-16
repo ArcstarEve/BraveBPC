90d9e519c22e0fb5ac10cd0ee2bb7c87b249dd39
ArcstarEve <arcstar.eve@gmail.com>
2020-05-16 15:05:01 -0700

    Move user database away from read-only location.
    The database was in a read-only location.  This change migrates it
    to a SQL server on GCP SQL.  I'm not sure I'm 100% happy with it
    right now, but it works so I'll go with it for now.

--- config.py
+++ config.py
@@ -1,9 +1,20 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+def gen_connection_string():
+    # if not on Google then use local MySQL
+    print("SERVER_SOFTWARE: {0}".format(os.getenv('SERVER_SOFTWARE', 'default')))
+    if not os.getenv('SERVER_SOFTWARE', '').startswith('gunicorn/'):
+        return 'sqlite:///' + os.path.join(basedir, 'app.db')
+    else:
+        conn_name = os.environ.get('CLOUDSQL_CONNECTION_NAME' '')
+        sql_user = os.environ.get('CLOUDSQL_USER', 'root')
+        sql_pass = os.environ.get('CLOUDSQL_PASSWORD', '')
+        conn_template = 'mysql://%s:%s@/bravebpc?unix_socket=/cloudsql/%s'
+        return conn_template % (sql_user, sql_pass, conn_name)
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = gen_connection_string()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AUTH_KEY = os.environ.get('AUTH_KEY') or 'you-will-never-guess'
